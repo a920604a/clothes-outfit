264f90bb7e539f7d98bb190d1263f7fd5c788a1b
a920604a <a920604a@gmail.com>
2024-06-15 07:22:08 +0800

    [Frontend] default page init

---
+++ frontend/app/src/MyComponent.js
@@ -0,0 +1,118 @@
+import styles from "./Frame.module.css";
+
+export const Frame = () => {
+    return (
+        <div className={styles.div}>
+            <div className={styles.navigation}>
+                <div className={styles.items}>
+                    <i className={styles.cooperateWithBeams}>Cooperate with Beams</i>
+                </div>
+                <div className={styles.styleChoose}>Style Choose</div>
+            </div>
+            <div className={styles.navigationFooter}>
+                <div className={styles.styleChoose1}>Style Choose</div>
+                <div className={styles.divider} />
+                <div className={styles.items1} />
+                <i className={styles.cooperateWithBeams1}>Cooperate with Beams</i>
+                <i className={styles.ccclubProject}>ccClub project</i>
+                <i className={styles.timvincentjennieellie}>
+                    Tim、Vincent、Jennie、Ellie
+                </i>
+                <img className={styles.image16Icon} alt="" src="/image-16@2x.png" />
+            </div>
+            <div className={styles.photo}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams}>Beams</div>
+            </div>
+            <div className={styles.photo1}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams}>Beams</div>
+            </div>
+            <div className={styles.photo2}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams}>Beams</div>
+            </div>
+            <div className={styles.photo3}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams}>Beams</div>
+            </div>
+            <div className={styles.photo4}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams}>Beams</div>
+            </div>
+            <div className={styles.photo5}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams}>Beams</div>
+            </div>
+            <div className={styles.photo6}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams}>Beams</div>
+            </div>
+            <div className={styles.photo7}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams}>Beams</div>
+            </div>
+            <div className={styles.photo8}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams8}>Beams</div>
+            </div>
+            <div className={styles.photo9}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams8}>Beams</div>
+            </div>
+            <div className={styles.photo10}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams8}>Beams</div>
+            </div>
+            <div className={styles.photo11}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.beams8}>Beams</div>
+            </div>
+            <div className={styles.style}>
+                <div className={styles.div1}>① 選擇地點的風格｜</div>
+                <div className={styles.div2}>工業</div>
+                <div className={styles.div3}>日系</div>
+                <div className={styles.div4}>極簡</div>
+                <div className={styles.div5}>古典</div>
+                <div className={styles.div6}>戶外</div>
+                <div className={styles.div7}>歐洲</div>
+            </div>
+            <div className={styles.gender}>
+                <div className={styles.div8}>男性</div>
+                <div className={styles.div2}>女性</div>
+                <div className={styles.div3}>不限性別</div>
+                <div className={styles.div1}>② 選擇性別｜</div>
+            </div>
+            <div className={styles.color}>
+                <div className={styles.div12}>③ 選擇顏色｜</div>
+                <img className={styles.colorChild} alt="" src="/vector-1.svg" />
+                <img className={styles.colorItem} alt="" src="/vector-2.svg" />
+                <img className={styles.colorInner} alt="" src="/vector-3.svg" />
+                <img className={styles.vectorIcon} alt="" src="/vector-4.svg" />
+                <img className={styles.colorChild1} alt="" src="/vector-5.svg" />
+                <img className={styles.colorChild2} alt="" src="/vector-6.svg" />
+                <img className={styles.colorChild3} alt="" src="/vector-7.svg" />
+                <img className={styles.colorChild4} alt="" src="/vector-8.svg" />
+            </div>
+            <div className={styles.div13}>
+                <div className={styles.div1}>1</div>
+                <div className={styles.div15}>2</div>
+                <div className={styles.div16}>3</div>
+                <div className={styles.div17}>4</div>
+                <div className={styles.div18}>5</div>
+                <div className={styles.div19}>{`>`}</div>
+            </div>
+            <div className={styles.div20}>
+                <div className={styles.div1}>1</div>
+                <div className={styles.div15}>2</div>
+                <div className={styles.div16}>3</div>
+                <div className={styles.div17}>4</div>
+                <div className={styles.div18}>5</div>
+                <div className={styles.div19}>{`>`}</div>
+            </div>
+            <i className={styles.popularity200}>Popularity (200)</i>
+        </div>
+    );
+};
+
+export default Frame;
